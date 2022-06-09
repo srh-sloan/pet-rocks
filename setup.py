0f6839bda355376feb3fea341b3ef5526b9826a4
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-06-09 16:13:21 +0100

    Added setup.py to create an egg

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='pet-rocks',
+    version='0.1.0',    
+    description='Package for making pet rocks',
+    url='ttps://github.com/srh-sloan/pet-rocks',
+    author='Sarah Sloan',
+    author_email='blah@example.com',
+    license='MIT',
+    packages=['rocks'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',     
+        'Programming Language :: Python :: 3',
+    ],
+)
