8254b63c394ac5422857acf118b83b0cbad785e1
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-06-09 16:18:05 +0100

    version 0.2.0 - rocks have shape

--- rocks/rock.py
+++ rocks/rock.py
@@ -2,6 +2,7 @@ from unicodedata import name
 
 
 class Rock:
-    def __init__(self, name:str):
+    def __init__(self, name:str, shape:str):
         self.name = name
+        self.shape = shape
     
